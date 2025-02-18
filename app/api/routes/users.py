557671e9499dd8cf1bbd7c0a47f1e3dfe2db5fbc
zainabbasi38 <zainabbasi7534@gmail.com>
2025-02-18 10:12:20 +0300

    authentication inventory

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -1,34 +1,66 @@
-from fastapi import APIRouter, status, Depends, HTTPException
+from fastapi import APIRouter, Response, status, Depends, HTTPException
 from sqlmodel import Session, select
 from app.core.db import db_session
 from app.models.users import User
+from app.api.utils.users_auth_utils import Auth
 
 user_router = APIRouter(prefix="/users", tags=["users"])
 
-@user_router.post("/", status_code= status.HTTP_201_CREATED)
-async def create_new_user(user_data: User, session: Session= Depends(db_session)): #called Action
+def get_user_auth(db_sesion : Session= Depends(db_session))->Auth:
+    return Auth(db_sesion)
+
+@user_router.post("/signup", status_code= status.HTTP_201_CREATED)
+async def create_new_user(user_data: User, user_auth: Auth= Depends(get_user_auth)): #called Action
+
+
     if not user_data.first_name or not user_data.email or not user_data.user_name or not user_data.last_name or not user_data.password:
         raise HTTPException(status_code= status.HTTP_422_UNPROCESSABLE_ENTITY, detail="Required fields")
     
     #for username confirmation
-    statement = select(User).where(User.user_name==user_data.user_name)
-    isusernameexist = session.exec(statement).first()
-    if isusernameexist:
+    user_name = await user_auth.get_user_by_useranme(user_data.user_name)
+    if user_name:
         raise HTTPException(status_code= status.HTTP_409_CONFLICT, detail="username exist, crate new account")
     
-    #for email verification
-    statement = select(User).where(User.email==user_data.email)
-    isemailexist = session.exec(statement).first()
-    if isemailexist:
+    #for email verification by get_user_email
+    user = await user_auth.get_user_by_email(user_data.email)
+    if user:
         raise HTTPException(status_code= status.HTTP_409_CONFLICT, detail="email exist, create new account")
-    
+    print(f"user generated password {user_data.password}")
+    hashed_password = user_auth.hash_password(user_data.password)
+    user_data.password  = hashed_password
+    print(f"user with hashed password { user_data}")
+
+
     data = user_data
 
-
-    session.add(data)
-    session.commit()
-    session.refresh(data)
+    user_auth.db_session.add(data)
+    user_auth.db_session.commit()
+    user_auth.db_session.refresh(data)
 
     if data is None:
         raise HTTPException(status_code= status.HTTP_500_INTERNAL_SERVER_ERROR, detail="internal server error")
     return {"status": True, "Message": "user created successfully", "user_data": data}
+
+
+@user_router.post("/signin")
+async def sign_in(response: Response, user_email: str, user_password: str, user_auth: Auth = Depends(get_user_auth)):
+    is_user_exist = user_auth.get_user_by_email(user_email)  # Removed await
+    if not is_user_exist:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User does not exist")
+
+    is_password_match = user_auth.verify_password(user_password, is_user_exist.password)
+    if not is_password_match:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect password")
+
+    user_id = is_user_exist.id
+    access_token, refresh_token = user_auth.create_tokens(user_id)
+
+    response.set_cookie(
+        key="inventory_refresh_token",
+        value=refresh_token,
+        httponly=False,
+        secure=True,
+        samesite="Strict"  # Fixed typo
+    )
+
+    return {"status": True, "message": "You are logged in successfully", "access_token": access_token, "user": is_user_exist}
---
+++ app/api/utils/users_auth_utils.py
@@ -0,0 +1,90 @@
+from datetime import datetime, timedelta, timezone
+from typing import Union
+import bcrypt
+import jwt
+from sqlmodel import Session, select
+from fastapi import Depends
+from app.core.db import db_session
+from app.models.users import User
+
+
+class Auth():
+    access_token_key = "gfjdftydfytdff"
+    refresh_token_key = "gfjdfjkguufuifuiftydfytdff"
+    def __init__(self, db_session : Session = Depends(db_session)):
+        self.db_session = db_session
+
+    async def get_user_by_email(self, email: str)->Union[User, None]:
+        print("Enail come ", email)
+
+        #transformation of data to run validation
+        email_in_lowercase = email.lower()
+        statement = select(User).where(User.email == email_in_lowercase)
+        #return ->> True or false but it depends on return data
+        isemailexist = self.db_session.exec(statement).first()
+        return isemailexist
+    
+    async def get_user_by_id(self, id: str)->Union[User, None]:
+
+        #transformation of data to run validation
+    
+        statement = select(User).where(User.email == id)
+        #return ->> True or false but it depends on return data
+        isidexist = self.db_session.exec(statement).first()
+        return isidexist
+    
+    async def get_user_by_useranme(self, user_name: str)->Union[User, None]:
+
+        #transformation of data to run validation
+    
+        statement = select(User).where(User.user_name == user_name)
+        #return ->> True or false but it depends on return data
+        isusernameexist = self.db_session.exec(statement).first()
+        return isusernameexist
+
+    @staticmethod
+    def hash_password(password: str) -> str:
+        # use bcrypt to hash password
+        # saltvalue = 10 
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+        return hashed_password.decode('utf-8')
+    
+    @staticmethod
+    def verify_password(password:str, hashed_password: str) -> bool:
+        is_password_match = bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
+        return is_password_match
+    #Assumptions
+    #programmer or coder must write assumptions
+    #create tokens access, refresh tokens
+    #user id
+    #create access and refresh tokens with JWT
+    #issue date,, expiry date
+    #return access tokens and refresh tokens in tuple
+
+    def create_tokens(self,user_id:str)->tuple[str, str]:
+        issued_at = datetime.now(timezone.utc)
+        access_token_expired_at = datetime.now(timezone.utc) + timedelta(hours=3)
+        refresh_token_expired_at = datetime.now(timezone.utc) + timedelta(days=7)
+        #create acccess tokens
+        access_token= jwt.encode(
+            {"sub": user_id, "exp": access_token_expired_at, "iat": issued_at},
+            self.access_token_key,
+            algorithm='HS256'
+        )
+        #create refresh token
+        refresh_token= jwt.encode(
+            {"sub": user_id, "exp": refresh_token_expired_at, "iat": issued_at},
+            self.refresh_token_key,
+            algorithm='HS256'
+        )
+
+        return access_token, refresh_token
+
+
+
+
+# user_auth = Auth()
+        
+#is classs is callable = yes
+# is object is callable = No
